3044b038076c8bfe8b54bf0198cf531bc4848e32
Hikaru <hikaru.ito.1995@gmail.com>
2016-09-26 14:29:22 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/**
+ *  Import Dependencies
+ */
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+const exec = require('child_process').exec
+
+/**
+ * Paths
+ */
+const paths = {
+  scss: './style/**.scss',
+  cssDir: './build/css',
+  src: './src/**/*'
+}
+
+/**
+ * Tasks
+ */
+gulp.task('browsersync', () => {
+  browserSync.init({
+    files: ['./build/**/**.**'],
+    server: {
+      baseDir: './build'
+    },
+    port: 9000
+  })
+})
+
+gulp.task('sass', ()=> {
+  gulp.src(paths.scss)
+  .pipe(sass())
+  .pipe(gulp.dest(paths.cssDir))
+  .pipe(reload({ stream: true }))
+})
+
+gulp.task('build', (cb) => {
+  exec('node index.js', (err) => {
+    cb(err)
+  })
+})
+
+gulp.task('watch', ['browsersync'], () => {
+  gulp.watch(paths.src, ['build'])
+  gulp.watch(paths.scss, ['sass'])
+})
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+layout: layout.html
+---
+### Leapcast
+
+[Stream](/projects/stream)
+![Logo](https://gyazo.com/bb7a116f0ad12244843ae086507be2e1.png)
